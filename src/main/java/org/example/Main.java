addb4495006861203ec3c830ae74972765883ca1
OmarTamer177 <Otamer95@gmail.com>
2024-04-20 14:51:23 +0200

    initialize project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Ahmed",10,10000);
+        System.out.println(emp.getId());
+    }
+}

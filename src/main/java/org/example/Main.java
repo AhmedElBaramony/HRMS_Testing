c7049fb761c35fce531c453c8c848e717e90573c
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-20 19:38:49 +0200

    Test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        double eval = Evaluation.FAIR.ordinal();
-        System.out.println(eval);
+        int x = 20;
+        System.out.println(x);
     }
 }

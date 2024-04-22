1e8f03c4b80bd8b6a18ae27414048d79be0e8aa1
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-22 21:18:13 +0200

    Login Page Designed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int x = 20;
         System.out.println(x);
     }

bb73dcd0dc5cb4ee8dc5febf698d774d56f96141
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-23 18:27:56 +0200

    LeaveRequestManagementPage Design is Done

---
+++ src/main/java/LeaveRequestManagementPage.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class LeaveRequestManagementPage {
+    private JPanel TitlePanel;
+    private JLabel EmployeePageLabel;
+    private JPanel BtnPanel;
+    private JButton ApproveBtn;
+    private JButton RejectBtn;
+    private JButton BackBtn;
+    private JPanel EmployeeInfoPanel;
+    private JLabel NameLabel;
+    private JLabel IDLabel;
+    private JLabel EmployeeName;
+    private JLabel EmployeeID;
+    private JLabel DepartmentLabel;
+    private JLabel EmployeeDepartment;
+    private JPanel LeaveRequestPage;
+    private JPanel RequestDetailsPanel;
+    private JLabel StartDateLabel;
+    private JLabel EndDateLabel;
+    private JLabel TypeLabel;
+    private JLabel StartDate;
+    private JLabel EndDate;
+    private JLabel RequestType;
+    private JButton NextRequestBtn;
+    private JButton PrevRequestBtn;
+}

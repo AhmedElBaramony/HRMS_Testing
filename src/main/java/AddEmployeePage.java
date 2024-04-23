e55c0de0d1d73506c441e82d2fdf22cabb89881c
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-23 17:18:09 +0200

    Add Employee Page V1

--- src/main/java/AddEmployeePage.java
+++ src/main/java/AddEmployeePage.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class AddEmployeePage {
+    private JPanel TitlePanel;
+    private JLabel EmployeePageLabel;
+    private JPanel InnputPanel;
+    private JLabel UsernameLabel;
+    private JTextField UsernameText;
+    private JPasswordField PasswordText;
+    private JButton LoginBtn;
 }

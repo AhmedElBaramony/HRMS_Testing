a215c01150dea6149d257e23e450a606d3ac9755
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-23 17:54:01 +0200

    ManageEmployeePage is done

---
+++ src/main/java/ManageEmployeePage.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class ManageEmployeePage {
+    private JPanel TitlePanel;
+    private JLabel ManageEmployeePageLabel;
+    private JPanel EmployeeIDPanel;
+    private JTextField IDText;
+    private JLabel EmployeeIDLabel;
+    private JButton SubmitBtn;
+    private JPanel ManageEmployeePage;
+    private JPanel BtnPanel;
+    private JButton SetPayrollBtn;
+    private JButton GetPayrollBtn;
+    private JButton FireEmployeeBtn;
+    private JButton SetEvaluationBtn;
+    private JButton GetEvaluationBtn;
+    private JButton ReturnBtn;
+    private JPanel EmployeeInfoPanel;
+    private JLabel NameLabel;
+    private JLabel IDLabel;
+    private JLabel EmployeeName;
+    private JLabel EmployeeID;
+    private JLabel DepartmentLabel;
+    private JLabel EmployeeDepartment;
+}

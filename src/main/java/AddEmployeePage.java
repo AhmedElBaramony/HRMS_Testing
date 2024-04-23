1def24e6bf1cd2884fffe534a738ef8953f041bd
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-23 17:23:41 +0200

    Add Employee Page V2

--- src/main/java/AddEmployeePage.java
+++ src/main/java/AddEmployeePage.java
@@ -2,10 +2,20 @@ import javax.swing.*;
 
 public class AddEmployeePage {
     private JPanel TitlePanel;
-    private JLabel EmployeePageLabel;
-    private JPanel InnputPanel;
+    private JLabel AddEmployeePageLabel;
+    private JPanel InputPanel;
+    private JLabel NameLabel;
+    private JTextField NameText;
+    private JButton AddBtn;
+    private JLabel IDLabel;
+    private JLabel DepartmentLabel;
     private JLabel UsernameLabel;
+    private JLabel PasswordLabel;
+    private JTextField IDText;
+    private JTextField DepartmentText;
     private JTextField UsernameText;
-    private JPasswordField PasswordText;
-    private JButton LoginBtn;
+    private JTextField PasswordText;
+    private JPanel BtnPanel;
+    private JButton ReturnBtn;
+    private JPanel AddEmployeePage;
 }

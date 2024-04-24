fde373237bfd7371ec5852360516dadbdfb589a9
Ahmed El-Baramony <ahmedelbaramonycess@gmail.com>
2024-04-24 03:53:53 +0200

    GetPayrollPage + SetPayrollPage (Design) Is done

---
+++ src/main/java/GetPayrollPage.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class GetPayrollPage {
+    private JPanel TitlePanel;
+    private JLabel GetPayrollTitle;
+    private JPanel PayrollInfoPanel;
+    private JLabel BaseSalaryLabel;
+    private JLabel BonusLabel;
+    private JLabel TaxLabel;
+    private JLabel DeductionLabel;
+    private JPanel BtnPanel;
+    private JButton BackBtn;
+    private JLabel BaseSalaryText;
+    private JPanel GetPayrollPage;
+    private JLabel BonusText;
+    private JLabel TaxText;
+    private JLabel DeductionText;
+}
---
+++ src/main/java/SetPayrollPage.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class SetPayrollPage {
+    private JLabel SetPayrollTitle;
+    private JPanel PayrollInputPanel;
+    private JLabel BaseSalaryLabel;
+    private JLabel BonusLabel;
+    private JLabel TaxLabel;
+    private JButton SubmitBtn;
+    private JTextField BaseSalaryText;
+    private JPanel SetPayrollPage;
+    private JLabel DeductionLabel;
+    private JTextField BonusText;
+    private JTextField TaxText;
+    private JTextField DeductionText;
+    private JPanel BtnPanel;
+    private JPanel TitlePanel;
+    private JButton BackBtn;
+}
